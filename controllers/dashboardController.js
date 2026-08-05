a65d43f087e4f9ceef61fd77ce60bb7b86a206ca
suriyakumar132001 <suryakumar132001@gmail.com>
2026-08-05 15:06:51 +0530

    changes

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -12,6 +12,51 @@ const ownerDashboard = async (req, res) => {
 
 }
 
+const adminDashboard = async (req, res) => {
+
+    res.json({
+
+        success:true,
+
+        message:"Welcome Admin",
+
+        user:req.user
+
+    })
+
+}
+
+const hrDashboard = async (req, res) => {
+
+    res.json({
+
+        success:true,
+
+        message:"Welcome HR",
+
+        user:req.user
+
+    })
+
+}
+
+const siteEngineerDashboard = async (req, res) => {
+
+    res.json({
+
+        success:true,
+
+        message:"Welcome Site Engineer",
+
+        user:req.user
+
+    })
+
+}
+
 module.exports={
-    ownerDashboard
+    ownerDashboard,
+    adminDashboard,
+    hrDashboard,
+    siteEngineerDashboard
 }
--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,25 +1,64 @@
-const express=require("express");
+const express = require("express");
 
-const router=express.Router();
+const router = express.Router();
 
-const protect=require("../middleware/authMiddleware");
+const protect = require("../middleware/authMiddleware");
 
-const authorize=require("../middleware/roleMiddleware");
+const authorize = require("../middleware/roleMiddleware");
 
-const {ownerDashboard}=require("../controllers/dashboardController");
+const {
+  ownerDashboard,
+  adminDashboard,
+  hrDashboard,
+  siteEngineerDashboard,
+} = require("../controllers/dashboardController");
 
 // Owner Only
 
 router.get(
+  "/owner",
 
-"/owner",
+  protect,
 
-protect,
+  authorize("owner"),
 
-authorize("owner"),
-
-ownerDashboard
-
+  ownerDashboard,
 );
 
-module.exports=router;
+// Admin Only
+
+router.get(
+  "/admin",
+
+  protect,
+
+  authorize("admin"),
+
+  adminDashboard,
+);
+
+// HR Only
+
+router.get(
+  "/hr",
+
+  protect,
+
+  authorize("hr"),
+
+  hrDashboard,
+);
+
+// Site Engineer Only
+
+router.get(
+  "/siteengineer",
+
+  protect,
+
+  authorize("siteengineer"),
+
+  siteEngineerDashboard,
+);
+
+module.exports = router;
