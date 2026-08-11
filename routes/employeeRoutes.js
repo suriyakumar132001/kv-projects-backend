3e98ba2bda7f4de583f21e45389e619cd066b50b
suriyakumar132001 <suryakumar132001@gmail.com>
2026-08-11 11:45:37 +0530

    emp route changed

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -1,56 +1,41 @@
-const express=require("express");
+const express = require("express");
 
-const router=express.Router();
+const router = express.Router();
 
-const protect=require("../middleware/authMiddleware");
-const authorize=require("../middleware/roleMiddleware");
+const protect = require("../middleware/authMiddleware");
+const authorize = require("../middleware/roleMiddleware");
 
 const {
-    createEmployee,
-    getEmployees,
-    getEmployee,
-    updateEmployee,
-    deleteEmployee
+  createEmployee,
+  getEmployees,
+  getEmployee,
+  updateEmployee,
+  deleteEmployee,
 } = require("../controllers/employeeController");
 
 // Create Employee
-router.post(
-"/",
-protect,
-authorize("owner","hr"),
-createEmployee
-);
+router.post("/", protect, authorize("owner", "hr"), createEmployee);
 
 // Get All Employees
 router.get(
-"/",
-protect,
-authorize("owner","admin","hr"),
-getEmployees
+  "/",
+  protect,
+  authorize("owner", "admin", "hr", "siteengineer"),
+  getEmployees,
 );
 
 // Get Single Employee
 router.get(
-"/:id",
-protect,
-authorize("owner","admin","hr"),
-getEmployee
+  "/:id",
+  protect,
+  authorize("owner", "admin", "hr", "siteengineer"),
+  getEmployee,
 );
 
 // Update Employee
-router.put(
-    "/:id",
-    protect,
-    authorize("owner", "hr"),
-    updateEmployee
-);
+router.put("/:id", protect, authorize("owner", "hr"), updateEmployee);
 
 // Delete Employee
-router.delete(
-    "/:id",
-    protect,
-    authorize("owner"),
-    deleteEmployee
-);
+router.delete("/:id", protect, authorize("owner"), deleteEmployee);
 
-module.exports=router;
+module.exports = router;
